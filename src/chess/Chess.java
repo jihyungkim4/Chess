5b1e5d3d4ced18ad1744b682d8fbca4630719c2f
jihyungkim4 <jihyung.kim4@gmail.com>
2025-08-04 19:14:45 -0400

    Display initial chess board on game start
    - Added getBoardState() method to Chess.java to retrieve current board without making a move
    - Modified PlayChess.java to show the starting position immediately after Chess.start()
    - Improves user experience by showing the board before the first move

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -5,31 +5,39 @@ import java.util.ArrayList;
 
 public class Chess {
 
-    enum Player { white, black }
-    
+	enum Player {
+		white, black
+	}
+
 	/**
 	 * Plays the next move for whichever player has the turn.
 	 * 
 	 * @param move String for next move, e.g. "a2 a3"
 	 * 
 	 * @return A ReturnPlay instance that contains the result of the move.
-	 *         See the section "The Chess class" in the assignment description for details of
+	 *         See the section "The Chess class" in the assignment description for
+	 *         details of
 	 *         the contents of the returned ReturnPlay instance.
 	 */
-	
-     static Board board;
 
-     public static ReturnPlay play(String move) {
-        return board.play(move);
+	static Board board;
+
+	public static ReturnPlay play(String move) {
+		return board.play(move);
 	}
-	
-	
+
 	/**
 	 * This method should reset the game, and start from scratch.
 	 */
 	public static void start() {
 		/* FILL IN THIS METHOD */
-        board = new Board();
+		board = new Board();
+	}
+
+	/**
+	 * Get the current board state without making a move
+	 */
+	public static ArrayList<ReturnPiece> getBoardState() {
+		return new ArrayList<ReturnPiece>(board.piecesOnBoard);
 	}
 }
-
